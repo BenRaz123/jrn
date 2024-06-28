fd829f4e1e192c2ebe6e1ae2b1b1d7f1169e0971
BenRaz123 <ben.raz2008@gmail.com>
2024-06-28 20:33:09 +0000

    Created zero security implementation of encryptor trait
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- src/encryptor.rs
+++ src/encryptor.rs
@@ -2,4 +2,22 @@ pub trait Encryptor {
     fn hash_password(password: &str) -> String;
     fn verify_password<'a>(hashed_password: &'a str, entered_password: &'a str) -> bool;
     fn encrypt_journal_entry<'a>(password: &'a str, entry: &'a str) -> String;
+    fn decrypt_journal_entry<'a>(password: &'a str, entry: &'a str) -> String;
+}
+
+pub struct ZeroSecurity;
+
+impl Encryptor for ZeroSecurity {
+    fn hash_password(password: &str) -> String {
+        password.into()
+    }
+    fn verify_password<'a>(hashed_password: &'a str, entered_password: &'a str) -> bool {
+        hashed_password == entered_password
+    }
+    fn encrypt_journal_entry<'a>(_password: &'a str, entry: &'a str) -> String {
+        entry.into()
+    }
+    fn decrypt_journal_entry<'a>(_password: &'a str, entry: &'a str) -> String {
+        entry.into()
+    }
 }
